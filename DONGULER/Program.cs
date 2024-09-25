ba60d9585332b2e6c5894bc8e8e4413b067289a5
BERKAN KARAYEL <berkan_karayel_284@hotmail.com>
2024-09-26 00:03:44 +0300

    .

---
+++ DONGULER/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DONGULER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isRaining = true;  // Yağmur yağıyor mu? Doğru.
+            bool isSunny = false;   // Hava güneşli mi? Yanlış.
+
+            if (isRaining)
+            {
+                Console.WriteLine("Şemsiyeni al.");
+            }
+            else
+            {
+                Console.WriteLine("Dışarı çıkabilirsin.");
+            }
+        }
+    }
+}
