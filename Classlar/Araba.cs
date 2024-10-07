3d5d47298909dd95eb1b8c0b6a855dfe8dbf2a93
Berkan Karayel <berkankarayel.147@gmail.com>
2024-10-07 21:29:26 +0300

    classlar

---
+++ Classlar/Araba.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlar
+{
+    public class Araba
+    {
+        // Arabanın özellikleri (Fields)
+        public string marka;
+        public string model;
+        public int yil;
+
+
+        // Arabanın bilgilerini gösteren metot
+
+        public void BilgileriGoster()
+        {
+
+            Console.WriteLine(string.Format("Araba : {0} {1} {2}", marka, model, yil));
+
+
+        }
+
+        // Araba hareket etmeye başlar
+        public void HareketeGec()
+        {
+            Console.WriteLine($"{marka} {model} harekete geçti!");
+            Console.ReadLine();
+        }
+    }
+
+
+    
+}
---
+++ Classlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Araba sınıfından bir nesne oluşturuyoruz
+            Araba araba1 = new Araba();
+
+            // Araba özelliklerini atıyoruz
+            araba1.marka = "Toyota";
+            araba1.model = "Corolla";
+            araba1.yil = 2020;
+            // Arabanın bilgilerini yazdırıyoruz
+            araba1.BilgileriGoster();
+            // Araba hareket ediyor
+            araba1.HareketeGec();
+
+        }
+    }
+}
