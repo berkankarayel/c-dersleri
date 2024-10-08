95f3bab35eec8f53bf17a4ae7da1d88d7a2e1df4
Berkan Karayel <berkankarayel.147@gmail.com>
2024-10-09 00:21:48 +0300

    ogrenci uygulaması

---
+++ OrnekUygulama/Ogrenci.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrnekUygulama
+{
+    internal class Ogrenci
+    {
+
+        // Öğrencinin bilgilerini tutacak private değişkenler
+
+        private int ogrenciNo;
+        private string isim;
+        private string soyisim;
+        private double vize1;
+        private double vize2;
+        private double final;
+        private string okulIsmi;
+
+
+        // Yapıcı metot (Constructor)
+
+
+        public Ogrenci(int ogrenciNo, string isim, string soyisim, double vize1, double vize2, double final, string okulIsmi)
+        {
+            this.ogrenciNo = ogrenciNo;
+            this.isim = isim;
+            this.soyisim = soyisim;
+            this.vize1 = vize1;
+            this.vize2 = vize2;
+            this.final = final;
+            this.okulIsmi = okulIsmi;
+        }
+
+        // 1. Öğrenci bilgilerini gösteren metot
+        public void OgrenciBilgileriniGoster()
+        {
+            Console.WriteLine($"\nÖğrenci No: {ogrenciNo}");
+            Console.WriteLine($"İsim: {isim}");
+            Console.WriteLine($"Soyisim: {soyisim}");
+            Console.WriteLine($"Vize1: {vize1}");
+            Console.WriteLine($"Vize2: {vize2}");
+            Console.WriteLine($"Final: {final}");
+        }
+
+        public void OgrenciOrtalamaBul()
+        {
+            double ortalama = (vize1 * 0.3) + (vize2 * 0.3) + (final * 0.4);
+            Console.WriteLine($"\nÖğrencinin not ortalaması: {ortalama:F2}");
+        }
+
+        // 3. Öğrencinin okulunu getiren metot
+        public void OkulGetir()
+        {
+            Console.WriteLine($"\nÖğrencinin okulu: {okulIsmi}");
+        }
+
+
+    }
+}
---
+++ OrnekUygulama/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrnekUygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Yeni bir öğrenci oluşturuyoruz (yapıcı metot ile)
+            Ogrenci ogrenci1 = new Ogrenci(12345, "Ahmet", "Yılmaz", 70, 80, 90, "XYZ Üniversitesi");
+
+            bool devamEdiyor = true;
+
+            while (devamEdiyor)
+            {
+                // Menü yazdır
+                Console.WriteLine("\nÖğrenci Sistemi:");
+                Console.WriteLine("1 - Öğrenci Bilgilerini Göster");
+                Console.WriteLine("2 - Öğrenci Ortalamasını Göster");
+                Console.WriteLine("3 - Öğrencinin Okulunu Öğren");
+                Console.WriteLine("4 - Çıkış Yap");
+                Console.Write("Seçiminizi yapın: ");
+
+                int secim = Convert.ToInt32(Console.ReadLine());
+
+                switch (secim)
+                {
+                    case 1:
+                        ogrenci1.OgrenciBilgileriniGoster();
+                        break;
+                    case 2:
+                        ogrenci1.OgrenciOrtalamaBul();
+                        break;
+                    case 3:
+                        ogrenci1.OkulGetir();
+                        break;
+                    case 4:
+                        Console.WriteLine("Çıkış yapılıyor...");
+                        devamEdiyor = false;
+                        break;
+                    default:
+                        Console.WriteLine("Geçersiz seçim, lütfen 1-4 arası bir seçim yapın.");
+                        break;
+                }
+
+            }
+        }
+    }
+}
