088fa05f70424320c4bee66ee32b66035c4eda1c
Berkan Karayel <berkankarayel.147@gmail.com>
2024-10-08 12:03:56 +0300

    Erisim Belirleyiciler(PublicPrivate)

---
+++ ErisimBelirleyiciler/Musteri.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErisimBelirleyiciler
+{
+    public class Musteri
+    {
+        public string isim;
+        public string soyisim;
+        public int maasDegeri;
+        public string cinsiyet;
+        private int yas;
+
+        public Musteri()
+        {
+            Console.WriteLine("Müşteri sınıfın yapıcı metotu çalıştı ");
+        }
+
+        public void musteriBilgileriGöster()
+        {
+            Console.WriteLine("İsmi :" +isim);
+            Console.WriteLine("Soyisim : " +soyisim);
+            Console.WriteLine("Maaş Değeri " +maasDegeri);
+            Console.WriteLine("cinsiyet :" +cinsiyet);
+            Console.WriteLine("Yaş değeri :" +yas);
+        }
+
+
+        private void isimSoyisimYazdir(string musteriisim,string musterisoyisim)
+        {
+            Console.WriteLine("Müşterinin ismi : " +musteriisim+"musterisoyisim :"+musterisoyisim);
+        }
+
+    }
+}
---
+++ ErisimBelirleyiciler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErisimBelirleyiciler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Musteri musteri1 = new Musteri();
+
+            musteri1.isim = "berkan";
+            musteri1.soyisim = "karayel";
+            musteri1.maasDegeri = 200000000;
+            musteri1.cinsiyet = "erkek";
+
+
+        }
+    }
+}
